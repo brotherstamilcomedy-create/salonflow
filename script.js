b545b1a02a144f17529042e719f82c1a2a51b02e
brotherstamilcomedy-create <brotherstamilcomedy@gmail.com>
2026-08-14 14:21:55 +0530

    Add booking button interaction

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const bookingButton = document.querySelector(".primary");
 
+bookingButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    alert("Welcome to SalonFlow! Booking system coming soon.");
+});
